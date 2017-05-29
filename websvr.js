02ccd1ce157fc457e13cabda20813956e8dd5977
Jake Kleine <jakeo@Jakes-MacBook-Pro.local>
2017-05-28 22:02:07 -0700

    adding the files

---
+++ websvr.js
@@ -0,0 +1,11 @@
+var http = require("http");
+
+http.createServer(function (request, response) {
+
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   response.end('Hello World, my name is Jake Kleine');
+}).listen(8001);
